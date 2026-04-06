cd9cefb03bdebfe9be73dc91a743d4c2e946553d
Agência Comunica <agenciacomunicaon@gmail.com>
2026-04-06 10:35:40 -0300

    Adiciona favicon na landing publica

--- resources/views/public/landing.blade.php
+++ resources/views/public/landing.blade.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Pós-Graduação em Fisioterapia em Ortopedia e Trauma – UFMG</title>
+<link rel="icon" type="image/png" href="{{ asset('images/Icone-FTO.png') }}">
 <style>
   :root {
     --green: #2565aa;
